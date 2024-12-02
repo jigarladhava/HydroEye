eb9b77e084bb752852e9ec0667f783ee9b1bfa33
= <=>
2024-12-02 16:07:48 +0530

    SWMS tag correction

--- dataReader.js
+++ dataReader.js
@@ -46,9 +46,29 @@ async function readTagsFromFile(location) {
 
 async function readTagsFromPCTFile(location) {
     try {
-        const tagsData = fs.readFileSync(`ph1_preload/allTagswithPCT.txt`, 'utf8');
-        let AlltagData = tagsData.trim().split('\r\n');
-        return AlltagData.filter(currenttag => currenttag.includes(location));
+        
+        const locationsData = fs.readFileSync('ph1_preload/prefix_locations.txt', 'utf8');
+        const allLines = locationsData.trim().split('\r\n');
+
+        // Find the line that starts with the given location
+        const matchingLine = allLines.find(line => line.startsWith(location + '|'));
+
+        if (matchingLine) {
+            // Return the part after the `|`
+            var prefix =  matchingLine.split('|')[1];
+            const tagsData = fs.readFileSync(`ph1_preload/allTagswithPCT.txt`, 'utf8');
+            let AlltagData = tagsData.trim().split('\r\n');
+            return AlltagData.filter(currenttag => currenttag.includes(prefix));
+
+        }
+
+        // Return null or a default value if no match is found
+        return null;
+
+
+
+
+
     } catch (error) {
         console.error('Error reading tags file for location', location, ':', error);
         return [];
