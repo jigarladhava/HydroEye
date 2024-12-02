3495f2673a49e04d8413b8571bcc15df7bbef48c
= <=>
2024-12-02 16:04:57 +0530

    locationName simplified

--- dataReader.js
+++ dataReader.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 
 async function readLocationsFromFile() {
     try {
-        const locationsData = fs.readFileSync('ph1_preload/locations.txt', 'utf8');
-        return locationsData.trim().split('\r\n');
+        const locationsData = fs.readFileSync('ph1_preload/prefix_locations.txt', 'utf8');
+
+        // return locationsData.trim().split('\r\n');
+        return locationsData.trim().split('\r\n').map(line => line.split('|')[0]);
     } catch (error) {
         console.error('Error reading locations file:', error);
         return [];
@@ -13,9 +15,29 @@ async function readLocationsFromFile() {
 
 async function readTagsFromFile(location) {
     try {
-        const tagsData = fs.readFileSync(`ph1_preload/allTags.txt`, 'utf8');
-        let AlltagData = tagsData.trim().split('\r\n');
-        return AlltagData.filter(currenttag => currenttag.includes(location));
+
+
+        /*   */
+
+        const locationsData = fs.readFileSync('ph1_preload/prefix_locations.txt', 'utf8');
+        const allLines = locationsData.trim().split('\r\n');
+
+        // Find the line that starts with the given location
+        const matchingLine = allLines.find(line => line.startsWith(location + '|'));
+
+        if (matchingLine) {
+            // Return the part after the `|`
+            var prefix =  matchingLine.split('|')[1];
+            const tagsData = fs.readFileSync(`ph1_preload/allTags.txt`, 'utf8');
+            let AlltagData = tagsData.trim().split('\r\n');
+            return AlltagData.filter(currenttag => currenttag.includes(prefix));
+
+        }
+
+        // Return null or a default value if no match is found
+        return null;
+
+
     } catch (error) {
         console.error('Error reading tags file for location', location, ':', error);
         return [];
